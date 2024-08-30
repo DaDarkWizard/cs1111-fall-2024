601582be028bd89518fd5eae8ef8bbfddf6f05f2
Dasker <dtmasker@mtu.edu>
2024-08-30 08:45:05 -0400

    Example from class 1.

---
+++ week1/hello.c
@@ -0,0 +1,15 @@
+/*
+ * Simple hello world program!
+ *
+ * @author Daniel Masker
+ * @date 8/29/2024
+ * @email dtmasker@mtu.edu
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
